bab6bf9898a84725a9abdfae8cd17660351cb045
Andrew Codispoti <andrewcod749@gmail.com>
2014-10-16 19:30:22 -0400

    20141016

--- poly.c
+++ poly.c
@@ -2,7 +2,8 @@
 #include<math.h>
 void polyPrint(double a[], int n) {
 	int i;
-	//adjust n for actual integer values
+	int empty = 1;
+	//adjust n for actual integer value
 	n--;
 	//first checks to make sure array is not empty
 	if (checkifempty(a, n)) {
@@ -11,40 +12,50 @@ void polyPrint(double a[], int n) {
 			//if the value is 0 dont print it and the value is not 1
 			if (a[i] != 1 && abs(a[i]) >= 0) {
 				// if the value is greater than 0 than print it
-				if (a[i] > 0)
+				if (a[i] > 0) {
 					printf("%g", a[i]);
+					empty = 0;
+				}
 				//if the value is less than 0 and its the first iteration
 				else if (a[i] < 0 && i == n) {
-					if (a[i] == -1)
+					if (a[i] == -1) {
 						printf("-");
-					else
+						empty = 0;
+					} else {
 						printf("%g", a[i]);
+						empty = 0;
+					}
 				}
 			}
 			// if the number has a greater exponent than 1 and value isnt 0
 			if (i > 1 && a[i] != 0) {
+				empty = 0;
 				printf("x^%d", i);
 			} else if (i == 1) {
 				//the cases if the value is x with exponent of 1
 				if (a[i] == 0)
 					continue;
-				else
+				else {
 					printf("x");
+					empty = 0;
+				}
 			}
-
-			if (i > 0 && abs(a[i]) > 0 && abs(a[i - 1]) > 0
-					&& checkifempty(a, i)) {
-				if (a[i] < 0)
-					printf(" - ");
-				else if (a[i] > 0)
-					printf(" + ");
+			//handle placing signs
+			if (i > 0 && abs(a[i - 1]) > 0 && !empty) {
+				if (checkifempty(a, i)) {
+					if (a[nextElement(a, i)] < 0)
+						printf(" - ");
+					else if (a[nextElement(a, i)] > 0)
+						printf(" + ");
+					i = nextElement(a, i)+1;
+				}
 			}
 
 		}
 	} else
 		printf("0");
 	//newline after creating polynomial
-printf("\n");
+	printf("\n");
 }
 //returns 1 if not empty and 0 if empty
 int checkifempty(double a[], int start) {
@@ -52,6 +63,14 @@ int checkifempty(double a[], int start) {
 	for (i = start - 1; i >= 0; i--) {
 		if (a[i] != 0.0)
 			return 1;
+	}
+	return 0;
+}
+int nextElement(double a[], int start) {
+	int i;
+	for (i = start - 1; i >= 0; i--) {
+		if (a[i] != 0.0)
+			return i;
 	}
 	return 0;
 }
--- polymain.c
+++ polymain.c
@@ -6,12 +6,24 @@ int main(){
   double d[] = {2.0, 0.0, 0.0, 0.0, 0.0, 1.0};
   double e[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   double f[] = {2.0/3.0, 1.0/7.0, 9.0/13.0};
-
+double g[]={5,666,77,4,3,2};
+double h[]={7,-32,-5,-22,-23,-95,-2345,-123};
+double i[]={3,2,3,0};
+double j[]={3,2,3,-1};
+double k[]={3,0.5,1.7,-6.8};
+double l[]={2,0,0,7};
   polyPrint (a, sizeof(a)/sizeof(a[0]));
   polyPrint (b, sizeof(b)/sizeof(b[0]));
   polyPrint (c, sizeof(c)/sizeof(c[0]));
   polyPrint (d, sizeof(d)/sizeof(d[0]));
   polyPrint (e, sizeof(e)/sizeof(e[0]));
   polyPrint (f, sizeof(f)/sizeof(f[0]));
+  polyPrint (g, sizeof(g)/sizeof(g[0]));
+  polyPrint (h, sizeof(h)/sizeof(h[0]));
+  polyPrint (i, sizeof(i)/sizeof(i[0]));
+  polyPrint (j, sizeof(j)/sizeof(j[0]));
+  polyPrint (k, sizeof(k)/sizeof(k[0]));
+  polyPrint (l, sizeof(l)/sizeof(l[0]));
+
   return 0;
 }
