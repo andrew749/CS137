78ff0e5f61a27414e4f3651368e5151b8e4f88bc
Andrew <andrewcod749@gmail.com>
2014-11-19 01:54:11 -0500

    5/7 working

--- sf.c
+++ sf.c
@@ -1,39 +1,80 @@
 #include<stdio.h>
 #include"sf.h"
+int debug=0;
+void sortCards(struct card hand[], int n);
 int straightflush(struct card hand[], int n){
+	int i=0,previous;
+	sortCards(hand,n);
+	int count=0;
+	char suit;
+	i=0;
+	while(i<n){
+		suit=hand[i].suit;
+		count=0;
+		while(hand[i].suit==suit)
+		{
 
+			for(;i<n&&hand[i].suit==suit;i++){
+
+				//skip over similar elements
+				while(hand[i].value==previous){
+					i++;
+				}
+				//checks if the previous element is 1 less than the current to
+				if(previous=hand[i].value-1/*account for special cases*/){
+					count++;
+				}
+				else{
+					count=0;
+					continue;
+				}
+				if(count==5)
+					return 1;
+				previous=hand[i].value;
+			}
+
+		}}
+	return 0;
 
 }
-struct card *sortSuits(struct card hand[], int n){
-int i,j=0;
-//sort based on suit
-//returns order c,d,h,s
-//filter out cards that are the same
-  struct card *sortedArray=(struct card *) calloc(n,sizeof(struct card));
-  //put the first element in the array
-  sortedArray[0]=hand[0];
-  //sets the lowest suit
-  char lowest=hand[0].suit;
-  //sorts based on suit
-  for (i=1;i<n;i++){
-    //if the element is out of order
-  while(hand[i].suit>sortedArray[j].suit&&j<n){
-    printf("hand at %d is %c, sorted Array %d suit=%c\n",i,hand[i].suit,j,sortedArray[j].suit);
-    j++;
-  }
-  sortedArray[j]=hand[i];
+
+void sortCards(struct card hand[], int n){
+	int i,j=0;
+	struct card temp;
+	int min;
+	// order c,d,h,s
+	//sorts based on suit
+	for (i=0;i<n-1;i++){
+		min=i;
+		for(j=i+1;j<n;j++)
+			if(hand[j].suit<hand[min].suit) min=j;
+		temp=hand[min];
+		hand[min]=hand[i];
+		hand[i]=temp;
+	}
+	i=0;j=0;
+	//print array
 
 
-}
 
-}
-int main(){
-  struct card hand1[] = {{4,'s'}, {9,'s'},{12,'c'},{11,'s'},{8,'s'},
-                           {6,'d'}, {3,'d'},{7,'s'},{10,'s'},{12,'d'}};
-  struct card s[]=sortSuits(hand1,10);
-  int i;
-  for(i=0;i<10;i++){
-    printf("Sorted: Card %c  %d",s[i]->suit,s[i]->value);
-  }
-  return 0;
+	while(i<n){
+		char suit=hand[i].suit;
+		min=i;
+		while(hand[i].suit==suit){
+			//sort the suit
+			for(j=i+1;j<n&&hand[j].suit==suit;j++){
+
+				if(hand[j].value<hand[min].value){
+					min=j;
+				}
+			}
+			temp=hand[min];
+			hand[min]=hand[i];
+			hand[i]=temp;
+			i++;
+			min=i;
+		}
+		//when not equal to suiit
+	}
+
 }
